7df631f3baca5752e1e6d48f6e6763154fb37721
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2020-02-15 21:23:21 +0100

    [master] setup project - finished

--- src/components/honey-speech/honey-speech.e2e.ts
+++ src/components/honey-speech/honey-speech.e2e.ts
@@ -1,20 +1,20 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('my-component', () => {
+describe('honey-speech', () => {
   it('renders', async () => {
     const page = await newE2EPage();
 
-    await page.setContent('<my-component></my-component>');
-    const element = await page.find('my-component');
+    await page.setContent('<honey-speech></honey-speech>');
+    const element = await page.find('honey-speech');
     expect(element).toHaveClass('hydrated');
   });
 
   it('renders changes to the name data', async () => {
     const page = await newE2EPage();
 
-    await page.setContent('<my-component></my-component>');
-    const component = await page.find('my-component');
-    const element = await page.find('my-component >>> div');
+    await page.setContent('<honey-speech></honey-speech>');
+    const component = await page.find('honey-speech');
+    const element = await page.find('honey-speech >>> div');
     expect(element.textContent).toEqual(`Hello, World! I'm `);
 
     component.setProperty('first', 'James');
