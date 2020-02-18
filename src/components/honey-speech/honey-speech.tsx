372631a1d455ab7ef71c48e2ee43871e74ee40fd
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2020-02-18 23:04:00 +0100

    [master] assetsPath changed

--- src/components/honey-speech/honey-speech.tsx
+++ src/components/honey-speech/honey-speech.tsx
@@ -7,7 +7,7 @@ import {Logger} from "./log-helper";
 @Component({
   tag: "honey-speech",
   styleUrl: "honey-speech.css",
-  assetsDirs: ["assets"],
+  assetsDirs: ["collection/assets"],
   shadow: true
 })
 export class HoneySpeech {
