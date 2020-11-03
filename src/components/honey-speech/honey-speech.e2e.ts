5d99bbb786a14874c45bd1744e2bb323f42e0db1
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-11-03 12:55:27 +0100

    [feature/events] structur changed

--- src/components/honey-speech/honey-speech.e2e.ts
+++ src/components/honey-speech/honey-speech.e2e.ts
@@ -39,9 +39,9 @@ describe('example', () => {
     // await page.setContent('<honey-speech id="speaker1" textids="3"></honey-speech><p id="3">test</p>');
     const startedEvent = await page.spyOnEvent('honeySpeakerStarted');
     // const element = await page.find('honey-speech');
-    await element.click('honey-speech');
+    await page.click('honey-speech');
     // await page.waitForChanges()
-    // await page.waitForEvent('honeySpeakerStarted');
+    await page.waitForEvent('honeySpeakerStarted');
     expect(startedEvent).toHaveReceivedEvent();
   });
 
