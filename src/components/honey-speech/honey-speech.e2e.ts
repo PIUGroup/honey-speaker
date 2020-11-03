fc55295e6687662d504a649954bc022ed2ca4767
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-11-03 19:57:37 +0100

    [feature/events] new test cases added

--- src/components/honey-speech/honey-speech.e2e.ts
+++ src/components/honey-speech/honey-speech.e2e.ts
@@ -7,40 +7,30 @@ describe('example', () => {
   let element: E2EElement;
 
   beforeEach(async () => {
-    // page = await newE2EPage();
-    page = await newE2EPage({html: `<honey-speech textids="3"></honey-speech><p id="3">test</p>`});
+    page = await newE2EPage({html: `<honey-speech audiolang="en" textids="3"></honey-speech><p id="3">test</p>`});
     element = await page.find('honey-speech');
-    //   Logger.disableLogging();
   });
 
-  it('should render a foo-component', async () => {
-    // const page = await newE2EPage();
-    // const el = await page.find('honey-speech');
-    // await page.setContent(`<honey-speech textids="3"></honey-speech><p id="3">test</p>`);
+  it('should render a component', async () => {
     expect(element).not.toBeNull();
   });
 
   it('renders correct styles', async () => {
-    // const page = await newE2EPage();
-    // await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
-    // const element = await page.find('honey-speech');
     expect(element).toHaveClass('hydrated');
   });
 
   it('has a11y attributes', async () => {
-    // const page = await newE2EPage();
-    // await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
-    // const element = await page.find('honey-speech');
     expect(element).toEqualAttribute('title', 'Vorlesen');
+    expect(element).toEqualAttribute('alt', 'Lautsprechersymbol zur Sprachausgabe');
+  });
+
+  xit('has correct audio properties', async () => {
+    expect(element).toEqual( 'en');
   });
 
   it('fire events', async () => {
-    // const page = await newE2EPage();
-    // await page.setContent('<honey-speech id="speaker1" textids="3"></honey-speech><p id="3">test</p>');
     const startedEvent = await page.spyOnEvent('honeySpeakerStarted');
-    // const element = await page.find('honey-speech');
     await page.click('honey-speech');
-    // await page.waitForChanges()
     await page.waitForEvent('honeySpeakerStarted');
     expect(startedEvent).toHaveReceivedEvent();
   });
