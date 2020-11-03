d723e3b6f6b35ab7f5dc4bfaeac39edbde18781c
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-11-03 12:48:21 +0100

    [feature/events] structur changed

--- src/components/honey-speech/honey-speech.e2e.ts
+++ src/components/honey-speech/honey-speech.e2e.ts
@@ -1,51 +1,51 @@
-import { newE2EPage } from '@stencil/core/testing';
-import {Logger} from "../../libs/log-helper";
+import {newE2EPage} from '@stencil/core/testing';
+import {E2EElement, E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
 
 describe('example', () => {
 
-  // beforeEach(async () => {
-  //   // page = await newE2EPage({ html: `<callback-test></callback-test>`});
-  //   // elm = await page.find('callback-test');
-  //   Logger.disableLogging();
-  // });
+  let page: E2EPage;
+  let element: E2EElement;
+
+  beforeEach(async () => {
+    // page = await newE2EPage();
+    page = await newE2EPage({html: `<honey-speech textids="3"></honey-speech><p id="3">test</p>`});
+    element = await page.find('honey-speech');
+    //   Logger.disableLogging();
+  });
 
   it('should render a foo-component', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<honey-speech textids="3"></honey-speech><p id="3">test</p>`);
-    const el = await page.find('honey-speech');
-    expect(el).not.toBeNull();
+    // const page = await newE2EPage();
+    // const el = await page.find('honey-speech');
+    // await page.setContent(`<honey-speech textids="3"></honey-speech><p id="3">test</p>`);
+    expect(element).not.toBeNull();
   });
 
   it('renders correct styles', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
-    const element = await page.find('honey-speech');
+    // const page = await newE2EPage();
+    // await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
+    // const element = await page.find('honey-speech');
     expect(element).toHaveClass('hydrated');
   });
 
   it('has a11y attributes', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
-    const element = await page.find('honey-speech');
+    // const page = await newE2EPage();
+    // await page.setContent('<honey-speech textids="3"></honey-speech><p id="3">test</p>');
+    // const element = await page.find('honey-speech');
     expect(element).toEqualAttribute('title', 'Vorlesen');
   });
 
   it('fire events', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<honey-speech id="speaker1" textids="3"></honey-speech><p id="3">test</p>');
+    // const page = await newE2EPage();
+    // await page.setContent('<honey-speech id="speaker1" textids="3"></honey-speech><p id="3">test</p>');
+    const startedEvent = await page.spyOnEvent('honeySpeakerStarted');
     // const element = await page.find('honey-speech');
-    // const startedEvent = await page.spyOnEvent('honeySpeakerStarted');
-    await page.click('honey-speech');
-    await page.waitForEvent('honeySpeakerStarted');
-    // expect(startedEvent).toHaveReceivedEvent();
+    await element.click();
+    // await page.waitForChanges()
+    // await page.waitForEvent('honeySpeakerStarted');
+    expect(startedEvent).toHaveReceivedEvent();
   });
 
 });
-
-
 
 
 // xdescribe('honey-speech', () => {
