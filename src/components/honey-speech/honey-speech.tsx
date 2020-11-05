73dfed9ab00f5d513490a8e96cb595fe2abf4738
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-11-05 21:56:11 +0100

    [feature/toggle] symbol speaker without noice changed

--- src/components/honey-speech/honey-speech.tsx
+++ src/components/honey-speech/honey-speech.tsx
@@ -280,11 +280,6 @@ export class HoneySpeech {
               stroke-width="5" stroke-linejoin="round"
               d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z">
             </path>
-            <path
-              id="air"
-              stroke="var(--speaker-color,black);" fill="none" stroke-width="5" stroke-linecap="round"
-              d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6">
-            </path>
           </svg>
         )}
       </Host>
