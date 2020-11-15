0d17e92c2577b5106323dd2120005686e25cec80
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-11-15 15:41:58 +0100

    [feature/#35-disableclass] disable class config added
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-speaker/speaker-options.ts
+++ src/components/honey-speaker/speaker-options.ts
@@ -1,5 +1,6 @@
 export interface SpeakerOptions {
-
+  disabledHostClass?:string
+  enabledHostClass?:string
   disabledTitleText?: string;
   pressedTitleText?: string;
   unpressedTitleText?: string;
